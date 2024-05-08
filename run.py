7ce25094571799c42e0e07c68875544cfc12db69
Kweku7k <mr.adumatta@gmail.com>
2024-05-08 13:13:30 +0000

    init to prod

--- run.py
+++ run.py
@@ -3,9 +3,6 @@ from app.models import *
 
 
 if __name__ == '__main__':
-    #DEBUG is SET to TRUE. Laughing out louyd CHANGE FOR PROD
-    # app.run(port=5000,debug=True)
-    # app.secret_key='12345'
-    app.run(host='0.0.0.0', debug=True)
+    app.run(port=5000, host='0.0.0.0', debug=True)
 
     
