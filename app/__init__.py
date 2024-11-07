5225c1a665bba20ec15171415bc1129805474215
kweku7k <mr.adumatta@gmail.com>
2024-11-07 16:03:25 +0000

    resolved meger

--- app/__init__.py
+++ app/__init__.py
@@ -9,11 +9,7 @@ import os
 app = Flask(__name__)
 
 
-<<<<<<< HEAD
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:new_password@45.222.128.55:5432/eligibility'
-=======
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('FORMS_DB_URL')
->>>>>>> 6b9e95a22e719fa0ebaa4f55e47ff42c65d75710
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = "Yorseestrinuecretsing"
 
